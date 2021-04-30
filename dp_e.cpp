e079be6d683d4463777530caea20d9df2a5b8e32
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-05-01 01:29:12 +0530

    Space optimize : O(n * value_sum) -> O(value_sum).

--- dp_e.cpp
+++ dp_e.cpp
@@ -1,13 +1,16 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
+
+#define int64 int64_t
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  const int kInf = 1e9 + 10;
+  const int64 kInf = 1e15;
 
   int n, capacity;
   cin >> n >> capacity;
@@ -18,22 +21,15 @@ int main() {
     cin >> w[i] >> v[i];
     value_sum += v[i];
   }
-  vector<vector<int>> best(n, vector<int>(value_sum + 1, kInf));
-  for (int i = 0; i < n; i++) {
-    best[i][0] = 0;
-  }
-  best[0][v[0]] = w[0];
-  for (int i = 1; i < n; i++) {
-    for (int val = 1; val <= value_sum; val++) {
-      best[i][val] = best[i - 1][val];
-      if (val < v[i]) continue;
-      best[i][val] = min(best[i][val], best[i - 1][val - v[i]] + w[i]);
+  vector<int64> small(value_sum + 1, kInf);
+  small[0] = 0;
+  for (int item = 0; item < n; item++) {
+    for (int value = value_sum; value >= v[item]; value--) {
+      small[value] = min(small[value], small[value - v[item]] + w[item]);
     }
   }
-  for (int val = value_sum; val >= 0; val--) {
-    if (best[n - 1][val] <= capacity) {
-      cout << val << "\n";
-      return 0;
-    }
-  }
+  const auto it = find_if(small.rbegin(), small.rend(), [&capacity](const int64 x) {
+    return x <= capacity;
+  });
+  cout << value_sum - distance(small.rbegin(), it) << "\n";
 }
