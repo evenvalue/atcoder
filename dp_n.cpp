4a280261cc29f3c92bf210bc7cb0a6f0c8cfa87a
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-05-08 20:55:40 +0530

    Replace prefix sums with function sum() and calculate cost once rather than O(n) times.

--- dp_n.cpp
+++ dp_n.cpp
@@ -17,20 +17,22 @@ int main() {
   for (int &x : a) {
     cin >> x;
   }
-  vector<int64> sz(n + 1);
-  for (int i = 1; i <= n; i++) {
-    sz[i] = sz[i - 1] + a[i - 1];
-  }
+  auto sum = [&a](const int l, const int r) -> int64 {
+    int64 s = 0;
+    for (int i = l; i <= r; i++) {
+      s += a[i];
+    }
+    return s;
+  };
   vector<vector<int64>> best(n, vector<int64>(n, kInf));
   for (int x = 0; x < n; x++) {
     best[x][x] = 0;
   }
   for (int len = 2; len <= n; len++) {
     for (int l = 0, r = len + l - 1; l < n and r < n; l++, r++) {
+      const int64 cost = sum(l, r);
       for (int cut = l; cut < r; cut++) {
-        const int64 szlt = sz[cut + 1] - sz[l];
-        const int64 szrt = sz[r + 1] - sz[cut + 1];
-        best[l][r] = min(best[l][r], best[l][cut] + best[cut + 1][r] + szlt + szrt);
+        best[l][r] = min(best[l][r], best[l][cut] + best[cut + 1][r] + cost);
       }
     }
   }
