db1ad6e47d016f48d9f8ef78750a4b8a98b0961d
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-08-09 21:59:58 +0530

    Add sorting problem.
    Booby Prize: https://atcoder.jp/contests/abc213/tasks/abc213_b

---
+++ abc213_b.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<pair<int, int>> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i].first;
+    a[i].second = i + 1;
+  }
+  sort(a.begin(), a.end());
+  cout << a[n - 2].second << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  //cin >> t;
+  while (t--) {
+    solve();
+  }
+}
