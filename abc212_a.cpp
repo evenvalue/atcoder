9c9e303810747e16837426ce807a9d9899717db5
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-08-10 14:48:28 +0530

    Add brute force problem.
    Alloy: https://atcoder.jp/contests/abc212/tasks/abc212_a

---
+++ abc212_a.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int g, s;
+  cin >> g >> s;
+  if (g > 0 and s > 0) {
+    cout << "Alloy\n";
+  } else {
+    cout << (s == 0 ? "Gold" : "Silver") << '\n';
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  //cin >> t;
+  while (t--) {
+    solve();
+  }
+}
