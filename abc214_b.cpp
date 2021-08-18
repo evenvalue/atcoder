11ceba042922750e153285e3d1e5dcbbc6231c43
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-08-18 15:37:13 +0530

    Add brute force problem.
    How many?: https://atcoder.jp/contests/abc214/tasks/abc214_b

---
+++ abc214_b.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int s, t;
+  cin >> s >> t;
+  int ans = 0;
+  for (int a = 0; a <= s; a++) {
+    for (int b = 0; b <= s - a; b++) {
+      for (int c = 0; c <= s - a - b; c++) {
+        ans += (a * b * c <= t);
+      }
+    }
+  }
+  cout << ans << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  //cin >> t;
+  while (t--) {
+    solve();
+  }
+}
