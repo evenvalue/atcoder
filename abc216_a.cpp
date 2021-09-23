036ea1dd574c87ec4e52c2a5ecd0a4bf66628997
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-09-23 13:04:46 +0530

    Add implementation problem.
    Signed Difficulty: https://atcoder.jp/contests/abc216/tasks/abc216_a

---
+++ abc216_a.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+void solve(const int TestCase) {
+  string s;
+  cin >> s;
+  const int x = stoi(s.substr(0, (s.size() == 3 ? 1 : 2)));
+  const int y = s.back() - '0';
+  if (0 <= y and y <= 2) {
+    cout << x << '-';
+  } else if (3 <= y and y <= 6) {
+    cout << x;
+  } else {
+    cout << x << '+';
+  }
+  cout << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  auto begin = std::chrono::high_resolution_clock::now();
+
+  int t = 1;
+  //cin >> t;
+  for (int tc = 1; tc <= t; tc++) {
+    solve(tc);
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+}
