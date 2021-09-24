82777c96498328fce12725e6bafb9cca10598602
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-09-24 17:13:58 +0530

    Add std::set problem.
    Cutting Woods: https://atcoder.jp/contests/abc217/tasks/abc217_d

---
+++ abc217_d.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+void solve(const int TestCase) {
+  int l;
+  cin >> l;
+  set<int> s({0, l});
+  int q;
+  cin >> q;
+  while (q--) {
+    int type, x;
+    cin >> type >> x;
+    if (type == 1) { //Cut
+      s.insert(x);
+    } else { //Find
+      const auto it = s.upper_bound(x);
+      cout << *it - *prev(it) << '\n';
+    }
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  const auto begin = std::chrono::high_resolution_clock::now();
+
+  int t = 1;
+  //cin >> t;
+  for (int tc = 1; tc <= t; tc++) {
+    solve(tc);
+  }
+
+  const auto end = std::chrono::high_resolution_clock::now();
+  const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+}
