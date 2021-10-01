3cdd5a6d8654ad2f914e12b2150caf2d445bdea6
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-10-01 10:57:40 +0530

    Add dsu problem.
    Sum of Maximum Weights: https://atcoder.jp/contests/abc214/tasks/abc214_d

---
+++ abc214_d.cpp
@@ -0,0 +1,111 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+class dsu {
+  const int n;
+  vector<int> e;
+  int comp = 0;
+
+  int pri_find(const int x) {
+    return e[x] < 0 ? x : (e[x] = find(e[x]));
+  }
+
+public:
+  explicit dsu(const int n) : n(n), e(n, -1), comp(n) {}
+
+  int find(const int x) {
+    assert(0 <= x and x < n);
+    return pri_find(x);
+  }
+
+  bool unite(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x == y) return false;
+    if (e[x] > e[y]) swap(x, y);
+    e[x] += e[y];
+    e[y] = x;
+    comp--;
+    return true;
+  }
+
+  bool same(int x, int y) {
+    return (find(x) == find(y));
+  }
+
+  int size(const int x) {
+    return -e[find(x)];
+  }
+
+  int operator[](const int x) {
+    assert(0 <= x and x < n);
+    return e[x];
+  }
+};
+
+struct edge {
+  int x, y, w;
+
+  bool operator<(const edge &other) const {
+    return w < other.w;
+  }
+};
+
+void solve(const int TestCase) {
+  int n;
+  cin >> n;
+  vector<edge> edges(n - 1);
+  for (edge &e : edges) {
+    cin >> e.x >> e.y >> e.w;
+    e.x--, e.y--;
+  }
+  sort(edges.begin(), edges.end());
+  dsu d(n);
+  int64 ans = 0;
+  for (const edge &e : edges) {
+    ans += d.size(e.x) * 1LL * d.size(e.y) * 1LL * e.w;
+    d.unite(e.x, e.y);
+  }
+  cout << ans << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  const auto begin = std::chrono::high_resolution_clock::now();
+
+  int t = 1;
+  //cin >> t;
+  for (int tc = 1; tc <= t; tc++) {
+    solve(tc);
+  }
+
+  const auto end = std::chrono::high_resolution_clock::now();
+  const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+}
