5814d3b98b6e7192b858de702909c132d87b2dda
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-11-22 01:25:43 +0530

    Add math problem.
    Semi Common Multiple: https://atcoder.jp/contests/abc150/tasks/abc150_d

---
+++ abc210_d.cpp
@@ -0,0 +1,121 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+using int64 = int64_t;
+using ld = long double;
+
+[[maybe_unused]] int readInt() {
+  int x;
+  cin >> x;
+  return x;
+}
+[[maybe_unused]] int64 readInt64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+[[maybe_unused]] string readString() {
+  string s;
+  cin >> s;
+  return s;
+}
+[[maybe_unused]] double readDouble() {
+  return stod(readString());
+}
+[[maybe_unused]] ld readLongDouble() {
+  return stold(readString());
+}
+template<typename T1, typename T2>
+[[maybe_unused]] pair<T1, T2> readPair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+[[maybe_unused]] vector<T> readVec(const int sz) {
+  vector<T> v(sz);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+[[maybe_unused]] vector<vector<T>> readVecVec(const int n, const int m) {
+  vector<vector<T>> a(n);
+  for (vector<T> &v : a) {
+    v = readVec<T>(m);
+  }
+  return a;
+}
+
+const int64 kInf64 = 2e18 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void none() {
+  cout << 0 << '\n';
+  exit(0);
+}
+
+int expo2(int x) { //returns the exponent of 2 in the prime factorisation of x.
+  int ans = 0;
+  while ((x & 1) == 0) {
+    ans++;
+    x /= 2;
+  }
+  return ans;
+}
+
+int64 lcm(const int x, const int y) {
+  return (x / gcd(x, y)) * 1LL * y;
+}
+
+void solution() {
+  const int n = readInt(), m = readInt();
+  vector<int> a = readVec<int>(n);
+  for (int &x : a) x /= 2;
+  const int foo = expo2(a[0]);
+  int64 l = a[0];
+  for (const int x : a) {
+    if (foo != expo2(x)) none();
+    l = lcm(l, x);
+    if (l > m) none();
+  }
+  const int mul = m / l;
+  cout << (mul + 1) / 2 << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  const auto begin = std::chrono::high_resolution_clock::now();
+
+  //  freopen(".in", "r", stdin);
+  //  freopen(".out", "w", stdout);
+
+  int t = 1;
+  //cin >> t;
+  for (int tc = 1; tc <= t; tc++) {
+    solution();
+  }
+
+  const auto end = std::chrono::high_resolution_clock::now();
+  const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+}
