8eb198e7169cdc16a9084ae0caf722a5764b88e5
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-11-27 23:19:31 +0530

    Add implementation problem.
    Hard Calculation: https://atcoder.jp/contests/abc229/tasks/abc229_b

---
+++ abc229_b.cpp
@@ -0,0 +1,103 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+using int64 = int64_t;
+using ld = long double;
+
+[[maybe_unused]] int readInt() {
+  int x;
+  cin >> x;
+  return x;
+}
+[[maybe_unused]] int64 readInt64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+[[maybe_unused]] string readString() {
+  string s;
+  cin >> s;
+  return s;
+}
+[[maybe_unused]] double readDouble() {
+  return stod(readString());
+}
+[[maybe_unused]] ld readLongDouble() {
+  return stold(readString());
+}
+template<typename T1, typename T2>
+[[maybe_unused]] pair<T1, T2> readPair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+[[maybe_unused]] vector<T> readVec(const int sz) {
+  vector<T> v(sz);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+[[maybe_unused]] vector<vector<T>> readVecVec(const int n, const int m) {
+  vector<vector<T>> a(n);
+  for (vector<T> &v : a) {
+    v = readVec<T>(m);
+  }
+  return a;
+}
+
+const int64 kInf64 = 2e18 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solution() {
+  int64 a = readInt64(), b = readInt64();
+  if (a < b) swap(a, b);
+  string num_a = to_string(a), num_b = to_string(b);
+  while (not num_a.empty() and not num_b.empty()) {
+    const int da = num_a.back() - '0', db = num_b.back() - '0';
+    if (da + db >= 10) {
+      cout << "Hard\n";
+      return;
+    }
+    num_a.pop_back(), num_b.pop_back();
+  }
+  cout << "Easy\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  const auto begin = std::chrono::high_resolution_clock::now();
+
+  //  freopen(".in", "r", stdin);
+  //  freopen(".out", "w", stdout);
+
+  int t = 1;
+  //cin >> t;
+  for (int tc = 1; tc <= t; tc++) {
+    solution();
+  }
+
+  const auto end = std::chrono::high_resolution_clock::now();
+  const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+}
