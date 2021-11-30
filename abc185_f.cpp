8a1d7ce960daa7e74707f578570f4d38c250139d
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-11-30 15:27:55 +0530

    Add segment tree problem.
    Range Xor Query: https://atcoder.jp/contests/abc185/tasks/abc185_f

---
+++ abc185_f.cpp
@@ -0,0 +1,174 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+using int64 = int64_t;
+using ld = long double;
+
+[[maybe_unused]] int readInt() {
+  int x;
+  cin >> x;
+  return x;
+}
+[[maybe_unused]] int64 readInt64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+[[maybe_unused]] string readString() {
+  string s;
+  cin >> s;
+  return s;
+}
+[[maybe_unused]] double readDouble() {
+  return stod(readString());
+}
+[[maybe_unused]] ld readLongDouble() {
+  return stold(readString());
+}
+template<typename T1, typename T2>
+[[maybe_unused]] pair<T1, T2> readPair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+[[maybe_unused]] vector<T> readVec(const int sz) {
+  vector<T> v(sz);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+[[maybe_unused]] vector<vector<T>> readVecVec(const int n, const int m) {
+  vector<vector<T>> a(n);
+  for (vector<T> &v : a) {
+    v = readVec<T>(m);
+  }
+  return a;
+}
+
+const int64 kInf64 = 2e18 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+namespace cp {
+template<class node, class F = std::function<node(const node &, const node &)>>
+class segtree {
+  int n = 0;
+  std::vector<node> t;
+  const F unite;
+
+  void build(const int x, const int l, const int r, const std::vector<node> &a) {
+    if (l == r) {
+      t[x] = a[l];
+      return;
+    }
+    const int mid = (l + r) / 2;
+    const int y = 2 * (mid - l + 1) + x;
+    build(x + 1, l, mid, a);
+    build(y, mid + 1, r, a);
+    t[x] = unite(t[x + 1], t[y]);
+  }
+
+  void update(const int x, const int l, const int r, const int p, const node &v) {
+    if (l == p and p == r) {
+      t[x] = v;
+      return;
+    }
+    const int mid = (l + r) / 2;
+    const int y = 2 * (mid - l + 1) + x;
+    if (p <= mid) {
+      update(x + 1, l, mid, p, v);
+    } else {
+      update(y, mid + 1, r, p, v);
+    }
+    t[x] = unite(t[x + 1], t[y]);
+  }
+
+  node query(const int x, const int l, const int r, const int ql, const int qr) {
+    if (ql <= l and r <= qr) {
+      return t[x];
+    }
+    const int mid = (l + r) / 2;
+    const int y = 2 * (mid - l + 1) + x;
+    if (qr <= mid) {
+      return query(x + 1, l, mid, ql, qr);
+    } else if (mid < ql) {
+      return query(y, mid + 1, r, ql, qr);
+    } else {
+      return unite(query(x + 1, l, mid, ql, qr), query(y, mid + 1, r, ql, qr));
+    }
+  }
+
+public:
+  segtree() = default;
+  explicit segtree(const int n, const node e, F f) : n(n), t(2 * n - 1, e), unite(std::move(f)) {}
+  explicit segtree(const std::vector<node> &a, F f) : n(a.size()), t(2 * (a.size()) - 1), unite(std::move(f)) {
+    build(0, 0, n - 1, a);
+  }
+
+  void set(const int p, const node &v) {
+    assert(0 <= p and p < n);
+    update(0, 0, n - 1, p, v);
+  }
+
+  node get(const int l, const int r) {
+    assert(0 <= l and l <= r and r < n);
+    return query(0, 0, n - 1, l, r);
+  }
+};
+}// namespace cp
+
+void solution() {
+  const int n = readInt(), q = readInt();
+  const vector<int> a = readVec<int>(n);
+  cp::segtree<int> st(a, [](const int l, const int r) {
+    return l ^ r;
+  });
+  for (int query = 0; query < q; query++) {
+    const int type = readInt();
+    if (type == 1) {
+      const int x = readInt() - 1, y = readInt();
+      st.set(x, st.get(x, x) ^ y);
+    } else {
+      const int x = readInt() - 1, y = readInt() - 1;
+      cout << st.get(x, y) << '\n';
+    }
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  const auto begin = std::chrono::high_resolution_clock::now();
+
+  //  freopen(".in", "r", stdin);
+  //  freopen(".out", "w", stdout);
+
+  int t = 1;
+  //cin >> t;
+  for (int tc = 1; tc <= t; tc++) {
+    solution();
+  }
+
+  const auto end = std::chrono::high_resolution_clock::now();
+  const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+}
