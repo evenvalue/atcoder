a524716db71aea9a2f75506bc3d0c908b9fe1e6e
evenvalue <naveenpkulkarni07@gmail.com>
2021-12-31 15:46:46 +0530

    Add greedy problem.
    Prediction and Restriction: https://atcoder.jp/contests/abc149/tasks/abc149_d

---
+++ abc149_d.cpp
@@ -0,0 +1,134 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+using int64 = int64_t;
+using ld = long double;
+
+[[maybe_unused]] int readInt() {
+  int x;
+  cin >> x;
+  return x;
+}
+[[maybe_unused]] int64 readInt64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+[[maybe_unused]] char readChar() {
+  char c;
+  cin >> c;
+  return c;
+}
+[[maybe_unused]] string readString() {
+  string s;
+  cin >> s;
+  return s;
+}
+[[maybe_unused]] double readDouble() {
+  return stod(readString());
+}
+[[maybe_unused]] ld readLongDouble() {
+  return stold(readString());
+}
+template<typename T1, typename T2>
+[[maybe_unused]] pair<T1, T2> readPair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+[[maybe_unused]] vector<T> readVec(const int sz) {
+  vector<T> v(sz);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+[[maybe_unused]] vector<vector<T>> readVecVec(const int n, const int m) {
+  vector<vector<T>> a(n);
+  for (vector<T> &v : a) {
+    v = readVec<T>(m);
+  }
+  return a;
+}
+
+const int64 kInf64 = 2e18 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solution() {
+  const int n = readInt(), k = readInt();
+  const int r = readInt(), s = readInt(), p = readInt();
+  const string bot = readString();
+
+  auto play = [](const char bot_hand) {
+    if (bot_hand == 'r') {
+      return 'p';
+    } else if (bot_hand == 'p') {
+      return 's';
+    } else {
+      return 'r';
+    }
+  };
+
+  auto points = [&](const char hand) {
+    if (hand == 'r') {
+      return r;
+    } else if (hand == 'p') {
+      return p;
+    } else {
+      return s;
+    }
+  };
+
+  vector<char> plays(n, '-');
+
+  auto last = [&](const int i) {
+    return (i < k ? '-' : plays[i - k]);
+  };
+
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    if (const char hand = play(bot[i]); last(i) != hand) {
+      ans += points(hand);
+      plays[i] = hand;
+    }
+  }
+  cout << ans << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  const auto begin = std::chrono::high_resolution_clock::now();
+
+//  freopen(".in", "r", stdin);
+//  freopen(".out", "w", stdout);
+
+  int t = 1;
+  //cin >> t;
+  for (int tc = 1; tc <= t; tc++) {
+    solution();
+  }
+
+  const auto end = std::chrono::high_resolution_clock::now();
+  const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+}
