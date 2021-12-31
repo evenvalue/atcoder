1f6145dcaf649cfc0c2a4032927da08524b24a7c
evenvalue <naveenpkulkarni07@gmail.com>
2021-12-31 15:25:46 +0530

    Add digit dp problem.
    FG operation: https://atcoder.jp/contests/abc220/tasks/abc220_d

---
+++ abc220_d.cpp
@@ -0,0 +1,113 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+using int64 = int64_t;
+using ld = long double;
+
+[[maybe_unused]] int readInt() {
+  int x;
+  cin >> x;
+  return x;
+}
+[[maybe_unused]] int64 readInt64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+[[maybe_unused]] char readChar() {
+  char c;
+  cin >> c;
+  return c;
+}
+[[maybe_unused]] string readString() {
+  string s;
+  cin >> s;
+  return s;
+}
+[[maybe_unused]] double readDouble() {
+  return stod(readString());
+}
+[[maybe_unused]] ld readLongDouble() {
+  return stold(readString());
+}
+template<typename T1, typename T2>
+[[maybe_unused]] pair<T1, T2> readPair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+[[maybe_unused]] vector<T> readVec(const int sz) {
+  vector<T> v(sz);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+[[maybe_unused]] vector<vector<T>> readVecVec(const int n, const int m) {
+  vector<vector<T>> a(n);
+  for (vector<T> &v : a) {
+    v = readVec<T>(m);
+  }
+  return a;
+}
+
+const int64 kInf64 = 2e18 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 998244353;
+
+void mod(int &x) {
+  x -= (x >= kMod ? kMod : 0);
+}
+
+void solution() {
+  const int n = readInt();
+  const vector<int> a = readVec<int>(n);
+  vector<vector<int>> ways(n, vector<int>(10));
+  ways[0][a[0]] = 1;
+  for (int i = 1; i < n; i++) {
+    for (int prev_digit = 0; prev_digit < 10; prev_digit++) {
+      mod(ways[i][(prev_digit + a[i]) % 10] += ways[i - 1][prev_digit]);
+      mod(ways[i][(prev_digit * a[i]) % 10] += ways[i - 1][prev_digit]);
+    }
+  }
+  for (int d = 0; d < 10; d++) {
+    cout << ways[n - 1][d] << '\n';
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  const auto begin = std::chrono::high_resolution_clock::now();
+
+//  freopen(".in", "r", stdin);
+//  freopen(".out", "w", stdout);
+
+  int t = 1;
+  //cin >> t;
+  for (int tc = 1; tc <= t; tc++) {
+    solution();
+  }
+
+  const auto end = std::chrono::high_resolution_clock::now();
+  const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+}
