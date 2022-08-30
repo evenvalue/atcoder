add1d089b4de064f9af167135c4c74abcf950b75
evenvalue <naveenpkulkarni07@gmail.com>
2022-08-31 00:55:48 +0530

    Add Segment Tree problem.
    Parenthesis Checking: https://atcoder.jp/contests/abc223/tasks/abc223_f

---
+++ abc223_f.cpp
@@ -0,0 +1,218 @@
+#include <bits/stdc++.h>
+#include "atcoder/all"
+using namespace std;
+using namespace atcoder;
+
+#ifdef evenvalue
+  #include "debug.h"
+  #define debug(...) print(#__VA_ARGS__, __VA_ARGS__)
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+
+template<class node, class F = std::function<node(const node &, const node &)>>
+class SegTree {
+  int n = 0;
+  std::vector<node> t;
+  F unite;
+
+  void build(const int x, const int l, const int r, const std::vector<node> &a) {
+    if (l == r) {
+      t[x] = a[l];
+      return;
+    }
+    const int mid = (l + r) / 2;
+    const int y = 2 * (mid - l + 1) + x;
+    build(x + 1, l, mid, a);
+    build(y, mid + 1, r, a);
+    t[x] = unite(t[x + 1], t[y]);
+  }
+
+  void update(const int x, const int l, const int r, const int p, const node &v) {
+    if (l == p and p == r) {
+      t[x] = v;
+      return;
+    }
+    const int mid = (l + r) / 2;
+    const int y = 2 * (mid - l + 1) + x;
+    if (p <= mid) {
+      update(x + 1, l, mid, p, v);
+    } else {
+      update(y, mid + 1, r, p, v);
+    }
+    t[x] = unite(t[x + 1], t[y]);
+  }
+
+  node query(const int x, const int l, const int r, const int ql, const int qr) const {
+    if (ql <= l and r <= qr) {
+      return t[x];
+    }
+    const int mid = (l + r) / 2;
+    const int y = 2 * (mid - l + 1) + x;
+    if (qr <= mid) {
+      return query(x + 1, l, mid, ql, qr);
+    } else if (mid < ql) {
+      return query(y, mid + 1, r, ql, qr);
+    } else {
+      return unite(query(x + 1, l, mid, ql, qr), query(y, mid + 1, r, ql, qr));
+    }
+  }
+
+  void debug_node(const int x, const vector<int> &path) const {
+    for (int i = 0; i < path.size(); i++) {
+      cerr << path[i] << (i == path.size() - 1 ? ": " : " -> ");
+    }
+    cerr << t[x] << '\n';
+  }
+
+  void print_tree(const int x, const int l, const int r, vector<int> path) const {
+    path.push_back(x);
+    if (l == r) {
+      debug_node(x, path);
+      return;
+    }
+    const int mid = (l + r) / 2;
+    const int y = 2 * (mid - l + 1) + x;
+    print_tree(x + 1, l, mid, path);
+    print_tree(y, mid + 1, r, path);
+    debug_node(x, path);
+  }
+
+public:
+  SegTree() = default;
+  explicit SegTree(const int n, const node e, F f) : n(n), t(2 * n - 1, e), unite(std::move(f)) {}
+  explicit SegTree(const std::vector<node> &a, F f) : n(a.size()), t(2 * (a.size()) - 1), unite(std::move(f)) {
+    build(0, 0, n - 1, a);
+  }
+
+  void set(const int p, const node &v) {
+    assert(0 <= p and p < n);
+    update(0, 0, n - 1, p, v);
+  }
+
+  [[nodiscard]] node get(const int l, const int r) const {
+    assert(0 <= l and l <= r and r < n);
+    return query(0, 0, n - 1, l, r);
+  }
+
+  void print_tree() const {
+    print_tree(0, 0, n - 1, {});
+    cerr << "----------\n\n";
+  }
+};
+
+struct node {
+  int open;
+  int close;
+};
+
+inline void solution() {
+  const int n = read::Int(), q = read::Int();
+  const string s = read::String();
+  vector<node> leaves(n);
+  for (int i = 0; i < n; i++) {
+    leaves[i] = (s[i] == '(' ? node({1, 0}) : node({0, 1}));
+  }
+  SegTree<node> st(leaves, [](const node &l, const node &r) {
+    node ans{};
+    ans.open = r.open + max(0, l.open - r.close);
+    ans.close = l.close + max(0, r.close - l.open);
+    return ans;
+  });
+  for (int query = 0; query < q; query++) {
+    const int type = read::Int();
+    const int l = read::Int() - 1;
+    const int r = read::Int() - 1;
+    if (type == 1) {
+      swap(leaves[l], leaves[r]);
+      st.set(l, leaves[l]);
+      st.set(r, leaves[r]);
+    } else {
+      const node x = st.get(l, r);
+      if (x.open == 0 and x.close == 0) {
+        cout << "Yes\n";
+      } else {
+        cout << "No\n";
+      }
+    }
+  }
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  // freopen(".in", "r", stdin);
+  // freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  //cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
