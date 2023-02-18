643afd65181b52c31eab9d2a4c5788530adce5e8
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2023-02-18 17:33:00 +0530

    Add dp problem.
    Integer Division: https://atcoder.jp/contests/abc288/tasks/abc288_f

---
+++ abc288_f.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+#include "atcoder/all"
+using namespace std;
+
+#ifdef evenvalue
+  #include "debug.h"
+  #define debug(...) print(#__VA_ARGS__, __VA_ARGS__)
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 998244353;
+using mint = atcoder::static_modint<kMod>;
+
+int digit(const char c) {
+  assert('0' <= c and c <= '9');
+  return c - '0';
+}
+
+inline void solution() {
+  const int n = read::Int();
+  const string x = read::String();
+
+  vector<mint> dp(n), sum(n);
+  dp[0] = digit(x[0]);
+  sum[0] = digit(x[0]) + 1;
+
+  for (int i = 1; i < n; i++) {
+    dp[i] = (dp[i - 1] * 10) + (digit(x[i]) * sum[i - 1]);
+    sum[i] = sum[i - 1] + dp[i];
+  }
+
+  cout << dp[n - 1].val() << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  //cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
