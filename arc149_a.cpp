82a0e1f6cbba992e8eb6455bb64fce18a46613af
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2023-02-20 16:51:28 +0530

    Add ad-hoc problem.
    Repdigit Number: https://atcoder.jp/contests/arc149/tasks/arc149_a

---
+++ arc149_a.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+#include "atcoder/all"
+using namespace std;
+
+#ifdef evenvalue
+  #include "debug.h"
+  #define debug(...) print(#__VA_ARGS__, __VA_ARGS__)
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+using mint = atcoder::static_modint<kMod>;
+
+inline void solution() {
+  const int n = read::Int();
+  const int m = read::Int();
+
+  vector<int64> mod(10);
+
+  int digits = -1, num = -1;
+  for (int i = 1; i <= 9; i++) {
+    mod[i] = i % m;
+    if (mod[i] == 0) digits = 1, num = i;
+  }
+
+  for (int len = 2; len <= n; len++) {
+    for (int i = 1; i <= 9; i++) {
+      mod[i] = (mod[i] * 10 + i) % m;
+      if (mod[i] == 0) digits = len, num = i;
+    }
+  }
+
+  if (digits == -1) {
+    cout << -1 << '\n';
+  } else {
+    cout << string(digits, num + '0') << '\n';
+  }
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cout << fixed << setprecision(10);
+
+  int testcases = 1;
+  //cin >> testcases;
+  while (testcases--) {
+    solution();
+  }
+}
